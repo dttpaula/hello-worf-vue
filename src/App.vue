fa3d1e52fd017964fafffb196003968b8af90396
as <as>
2021-01-26 11:09:46 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App - Demétrius Tavares RM - 338132 "/>
   </div>
 </template>
 
